b628a2c6c471e02f229d4f910136fa66b3c9dcc1
Robbie Harwood <rharwood@club.cc.cmu.edu>
2016-08-17 19:51:43 -0400

    We ls now

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+use std::io::*;
+use std::process::*;
+
 fn main() {
-  println!("Hello, world!");
+    let mut nh = Command::new("ls")
+        .stdin(Stdio::piped())
+        .stdout(Stdio::piped())
+        .spawn()
+        .expect("Failed to execute child");
+
+    let mut res = vec![];
+
+    let mut stdin = nh.stdin.unwrap();
+    let mut stdout = nh.stdout.unwrap();
+
+    stdout.read_to_end(&mut res);
+    println!("{}", String::from_utf8(res).unwrap());
 }
