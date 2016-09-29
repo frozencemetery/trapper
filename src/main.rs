7c44e6952f06ad8011249265ab2da76d8e90714f
Robbie Harwood <rharwood@club.cc.cmu.edu>
2016-09-28 21:02:56 -0400

    Binding to libtsm

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,22 @@
 use std::io::*;
-use std::process::*;
+use std::process::{Command, Stdio};
+
+mod tsm;
 
 fn main() {
-    let mut nh = Command::new("ls")
+    let nh = Command::new("nethack")
         .stdin(Stdio::piped())
         .stdout(Stdio::piped())
         .spawn()
-        .expect("Failed to execute child");
+        .expect("Failed to execute child")
+        ;
+
+    let mut stdout = nh.stdout.expect("No stdout...?");
+    let mut stdin = nh.stdin.expect("No stdin...?");
+
+    stdin.write(b"y  #quit\nyq");
 
     let mut res = vec![];
-
-    let mut stdin = nh.stdin.unwrap();
-    let mut stdout = nh.stdout.unwrap();
-
     stdout.read_to_end(&mut res);
     println!("{}", String::from_utf8(res).unwrap());
 }
---
+++ src/tsm.rs
@@ -0,0 +1,30 @@
+/* here be FFI gunk */
+#![allow(non_camel_case_types)]
+
+#![allow(unused_variables)]
+#![allow(dead_code)]
+
+extern crate libc;
+use self::libc::*;
+
+#[link(name = "tsm")]
+#[repr(C)]
+pub struct tsm_screen {
+    pub _hacks: u8,
+}
+
+pub type tsm_log_t = Option<unsafe extern "C"
+                            fn(data: *mut c_void,
+                               file: *mut c_char,
+                               line: c_int,
+                               func: *const c_char,
+                               subs: *const c_char,
+                               sev: c_uint,
+                               format: *const c_char,
+                               ...)>;
+
+extern {
+    pub fn tsm_screen_new(out: *mut *mut tsm_screen,
+                          log: tsm_log_t,
+                          log_data: *mut c_void) -> c_int;
+}
