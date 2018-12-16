54bb7af67a00d758fa0314912872074988643c70
Robbie Harwood <rharwood@club.cc.cmu.edu>
2018-12-15 22:59:24 -0500

    temp commit of some kind?

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,4 @@
-use std::io::*;
-use std::process::{Command, Stdio};
-
+use std::ptr;
 mod tsm;
 
 extern crate libc;
@@ -19,19 +17,206 @@ fn shit_strerror() {
 }
 
 fn main() {
-    let nh = Command::new("nethack")
-        .stdin(Stdio::piped())
-        .stdout(Stdio::piped())
-        .spawn()
-        .expect("Failed to execute child")
-        ;
+    let mut screen = ptr::null_mut();
+    let err = unsafe {
+        tsm::tsm_screen_new(&mut screen, None, ptr::null_mut())
+    };
+    assert_eq!(0, err);
 
-    let mut stdout = nh.stdout.expect("No stdout...?");
-    let mut stdin = nh.stdin.expect("No stdin...?");
+    let err = unsafe { tsm::tsm_screen_resize(screen, 80, 24) };
+    assert_eq!(0, err);
 
-    stdin.write(b"y  #quit\nyq");
+    let res = unsafe { tsm::tsm_screen_get_width(screen) };
+    assert_eq!(80, res);
 
-    let mut res = vec![];
-    stdout.read_to_end(&mut res);
-    println!("{}", String::from_utf8(res).unwrap());
+    let res = unsafe { tsm::tsm_screen_get_height(screen) };
+    assert_eq!(24, res);
+
+    let attr = tsm::tsm_screen_attr {
+        fccode: 0,
+        bccode: 0,
+        fr: 0,
+        fg: 0,
+        fb: 0,
+        br: 0,
+        bg: 0,
+        bb: 0,
+        _bitfield: 0,
+    };
+    for c in "hello world".chars() {
+        unsafe { tsm::tsm_screen_write(screen, c as tsm::tsm_symbol_t, &attr); }
+    }
+
+    struct Output { string: String }
+    extern "C" fn draw_cb(_: *mut tsm::tsm_screen,
+                          _: u32,
+                          ch: *const u32,
+                          _: usize,
+                          _: libc::c_uint,
+                          _: libc::c_uint,
+                          _: libc::c_uint,
+                          _: *const tsm::tsm_screen_attr,
+                          _: tsm::tsm_age_t,
+                          output: *mut libc::c_void) -> libc::c_int {
+        let output: &mut Output = unsafe { &mut *(output as *mut Output) };
+        let c;
+        unsafe {
+            if *ch == 0 {
+                return 0;
+            } else {
+                c = from_u32(*ch).unwrap()
+            }
+        };
+        output.string.push(c);
+        0
+    }
+
+
+    extern "C" fn write_cb(_: *mut tsm::tsm_vte,
+                           _: *const libc::c_char,
+                           _: isize,
+                           _: *mut libc::c_void) {};
+    let mut vte = ptr::null_mut();
+    let r = unsafe { tsm::tsm_vte_new(&mut vte, screen, Some(write_cb),
+                                      ptr::null_mut(), None, ptr::null_mut()) };
+    assert_eq!(0, r);
+
+    let fd = unsafe {
+        libc::posix_openpt(libc::O_RDWR | libc::O_NOCTTY |
+                           libc::O_CLOEXEC | libc::O_NONBLOCK)
+    };
+    assert!(fd > 0);
+
+    let mut files: Vec<libc::c_int> = vec![0, 0, 0];
+    let r = unsafe { libc::pipe2(files.as_mut_ptr(), libc::O_CLOEXEC) };
+    assert!(r == 0);
+
+    let r = unsafe { libc::fork() };
+    assert!(r >= 0);
+    if r == 0 {
+        /* child */
+        unsafe { libc::close(files[0]) };
+
+        /* TODO(frozen) might need this? */
+        /* let mut sigset; */
+        /* unsafe { */
+        /*     /\* hello there FFI *\/ */
+        /*     sigset = std::mem::uninitialized(); */
+        /*     libc::sigemptyset(&mut sigset); */
+        /* }; */
+
+        /* let r = unsafe { */
+        /*     libc::pthread_sigmask(libc::SIG_SETMASK, &mut sigset, */
+        /*                           ptr::null_mut()) */
+        /* }; */
+        /* assert!(r == 0); */
+
+        /* for i in 1..libc::SIGUNUSED { */
+        /*     let r = unsafe { libc::signal(i, libc::SIG_DFL) }; */
+        /*     if r == libc::SIG_ERR { */
+        /*         println!("Signal error: {}", i); */
+        /*     } */
+        /* } */
+
+        let r = unsafe { libc::grantpt(fd) };
+        assert!(r == 0);
+
+        let r = unsafe { libc::unlockpt(fd) };
+        assert!(r == 0);
+
+        let slave_name = unsafe { libc::ptsname(fd) };
+        assert!(slave_name != 0 as *mut i8);
+
+        let slave = unsafe {
+            libc::open(slave_name,
+                       libc::O_RDWR | libc::O_CLOEXEC | libc::O_NOCTTY)
+        };
+        assert!(slave >= 0);
+
+        let pid = unsafe { libc::setsid() };
+        assert!(pid >= 0);
+
+        let r = unsafe { libc::ioctl(slave, libc::TIOCSCTTY, 0) };
+        assert!(r >= 0);
+
+        unsafe { libc::close(fd) };
+
+        let mut attr;
+        let r = unsafe {
+            attr = std::mem::uninitialized();
+            libc::tcgetattr(slave, &mut attr)
+        };
+        assert!(r >= 0);
+
+        /* TODO(frozen) the heck is this */
+        attr.c_cc[libc::VERASE] = 0o10;
+
+        let r = unsafe { libc::tcsetattr(slave, libc::TCSANOW, &mut attr) };
+        assert!(r >= 0);
+
+        let r = unsafe {
+            let mut ws: libc::winsize = std::mem::zeroed();
+            ws.ws_col = 80;
+            ws.ws_row = 24;
+            libc::ioctl(slave, libc::TIOCSWINSZ, &mut ws)
+        };
+        assert!(r >= 0);
+
+        let r = unsafe { libc::dup2(slave, libc::STDIN_FILENO) };
+        assert!(r != -1);
+        let r = unsafe { libc::dup2(slave, libc::STDOUT_FILENO) };
+        assert!(r != -1);
+        let r = unsafe { libc::dup2(slave, libc::STDERR_FILENO) };
+        assert!(r != -1);
+
+        if slave > 2 {
+            unsafe { libc::close(slave) };
+        }
+
+        /* sup, parent */
+        let d = "k".as_ptr();
+        /* TODO(frozen) handle failures better */
+        let mut r = 0;
+        while r == 0 {
+            r = unsafe { libc::write(files[1], d as *const libc::c_void, 1) };
+        }
+        assert!(r == 1);
+
+        unsafe { libc::close(files[1]) };
+
+        /* IT'S TIME TO PLAY THE GAME */
+        let env = ["TERM=xterm-256color\0".as_ptr() as *const i8,
+                   ptr::null_mut()];
+        let argv = ["/usr/bin/nethack\0".as_ptr() as *const i8,
+                    ptr::null_mut()];
+        let _ = unsafe { libc::execve(argv[0], &argv[0], &env[0]) };
+        assert!(false);
+    } else {
+        /* parent */
+        unsafe { libc::close(files[1]) };
+
+        let mut r = 0;
+        let mut d: u8 = 0;
+        while r == 0 {
+            unsafe {
+                r = libc::read(files[0], std::mem::transmute(&mut d), 1);
+            }
+            println!("{:?}", d);
+        }
+        assert!(r == 1);
+        assert!(d == 'k' as u8);
+
+        let mut output = Output { string: "".to_string() };
+        let output_ptr = &mut output as *mut _ as *mut libc::c_void;
+        unsafe { tsm::tsm_screen_draw(screen, Some(draw_cb), output_ptr) };
+        println!("{:?}", &output.string);
+    }
+
+    /* stdin.write(b"y  #quit\nyq"); */
+
+    /* loop { */
+    /*     let mut res = vec![]; */
+    /*     stdout.read(&mut res); */
+    /*     println!("{}", String::from_utf8(res).unwrap()); */
+    /* } */
 }
--- src/tsm.rs
+++ src/tsm.rs
@@ -12,19 +12,86 @@ pub struct tsm_screen {
     pub _hacks: u8,
 }
 
-pub type tsm_log_t = Option<unsafe extern "C"
-                            fn(data: *mut c_void,
-                               file: *mut c_char,
-                               line: c_int,
-                               func: *const c_char,
-                               subs: *const c_char,
-                               sev: c_uint,
-                               format: *const c_char,
-                               ...)>;
+pub type tsm_log_t = Option<
+        extern "C" fn(data: *mut c_void,
+                      file: *mut c_char,
+                      line: c_int,
+                      func: *const c_char,
+                      subs: *const c_char,
+                      sev: c_uint,
+                      format: *const c_char,
+                      ...)>;
+
+#[repr(C)]
+/* #[derive(Debug, Clone, Copy)] */
+pub struct tsm_screen_attr {
+    pub fccode: i8,
+    pub bccode: i8,
+    pub fr: u8,
+    pub fg: u8,
+    pub fb: u8,
+    pub br: u8,
+    pub bg: u8,
+    pub bb: u8,
+    pub _bitfield: u8,
+}
+
+/* libtsm is 32-bit wide internally */
+pub type tsm_symbol_t = u32;
+
+/* There's no explanation for this one, and it isn't quite right anyway */
+pub type tsm_age_t = u32;
+
+pub type tsm_screen_draw_cb = Option<
+        extern "C" fn(con: *mut tsm_screen,
+                      id: u32,
+                      ch: *const u32,
+                      len: usize,
+                      width: c_uint,
+                      posx: c_uint,
+                      posy: c_uint,
+                      attr: *const tsm_screen_attr,
+                      age: tsm_age_t,
+                      data: *mut c_void) -> c_int>;
 
 #[link(name = "tsm")]
 extern {
     pub fn tsm_screen_new(out: *mut *mut tsm_screen,
                           log: tsm_log_t,
                           log_data: *mut c_void) -> c_int;
+    pub fn tsm_screen_resize(con: *mut tsm_screen,
+                             x: c_uint,
+                             y: c_uint) -> c_int;
+    pub fn tsm_screen_get_width(con: *mut tsm_screen) -> c_uint;
+    pub fn tsm_screen_get_height(con: *mut tsm_screen) -> c_uint;
+    pub fn tsm_screen_write(con: *mut tsm_screen,
+                            ch: tsm_symbol_t,
+                            attr: *const tsm_screen_attr);
+    pub fn tsm_screen_draw(con: *mut tsm_screen,
+                           draw_cb: tsm_screen_draw_cb,
+                           data: *mut c_void) -> tsm_age_t;
+}
+
+#[repr(C)]
+pub struct tsm_vte {
+    pub _hacks: u8,
+}
+
+pub type tsm_vte_write_cb = Option<
+        extern "C" fn(vte: *mut tsm_vte,
+                      u8: *const c_char,
+                      len: isize,
+                      data: *mut c_void)>;
+
+#[link(name = "tsm")]
+extern {
+    pub fn tsm_vte_new(out: *mut *mut tsm_vte,
+                       con: *mut tsm_screen,
+                       write_cb: tsm_vte_write_cb,
+                       data: *mut c_void,
+                       log: tsm_log_t,
+                       log_data: *mut c_void) -> c_int;
+    pub fn tsm_vte_input(vte: *mut tsm_vte,
+                         u8: *const c_char,
+                         len: isize);
 }
