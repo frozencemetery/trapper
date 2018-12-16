1522e4102c0bc3f698727b04f7052802dce97b6a
Robbie Harwood <rharwood@club.cc.cmu.edu>
2018-12-16 01:29:58 -0500

    It's playable, just kind of unpleasant

--- trapper.py
+++ trapper.py
@@ -5,6 +5,8 @@ import pty
 import select
 import signal
 import sys
+import termios
+import tty
 
 import pyte
 
@@ -15,18 +17,35 @@ if __name__ == "__main__":
     pid, amaster = pty.fork()
     if pid == 0: # chile
         os.execvpe("/usr/games/nethack", ["/usr/games/nethack"],
-                   env=dict(TERM="linux", COLUMNS="78", LINES="24"))
+                   env=dict(TERM="linux", COLUMNS="78", LINES="24",
+                            NETHACKOPTIONS=f"{os.getenv('HOME')}/.nethackrc"))
         exit(-1)
 
+    # python3 broke setting stdin unbuffered.  Thanks, python3.
+    stdin = os.fdopen(sys.stdin.fileno(), "rb", buffering=0)
+    tty.setcbreak(stdin, termios.TCSANOW)
+
+    inqueue = []
     while True:
-        rl, wl, xl = select.select([amaster], [], [])
-        if amaster in xl:
+        wl = [] if len(inqueue) == 0 else [amaster]
+        rl, wl, xl = select.select([amaster, stdin], wl, [amaster, stdin])
+        if xl != []:
             break
-        elif amaster not in rl: # u wot
+        elif amaster in rl:
+            data = os.read(amaster, 1024)
+            stream.feed(data)
+            print(*screen.display, sep="\n")
+            continue
+        elif amaster in wl:
+            elt = inqueue.pop(0)
+            os.write(amaster, elt)
+            continue
+        elif stdin in rl:
+            elt = stdin.read(1)
+            inqueue.append(elt)
             continue
 
-        stream.feed(os.read(amaster, 1024))
-        print(*screen.display, sep="\n")
+        break
 
     print(*screen.display, sep="\n")
     os.kill(pid, signal.SIGTERM)
